d06ed5742baf652db52441579ae1286d9bdf93e6
christian <christiancamilo1221@gmail.com>
2018-03-05 11:48:12 -0500

    confirmando codigos

---
+++ potosi vela.php
@@ -0,0 +1,12 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Practicando Git </title>
+   
+</head>
+<body>
+    <?php echo '<p>Hola Mundo con Git</p>' ?>
+</body>
+</html>
