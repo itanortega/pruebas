ab543d19714a5302aea63dd2bc23c71706530ec7
Esteban Potosi <johnpoto01@hotmail.com>
2018-03-06 11:48:06 -0500

    789

--- holaEstebap.php
+++ holaEstebap.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Pagina de Prueba Esteban.</title>
+    <title>Página de Prueba Esteban.</title>
    
 </head>
 <body>
